2bd880acc0987157c247f1ab55ea45bf3f7ee9e6
slawek <slawek@dom-11b457819a9>
2013-04-18 16:56:46 +0200

    Dodanie pierwszego projektu do SzkolenieJava

---
+++ Hello/src/hello/Hello.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import javax.xml.transform.Source;
+
+/**
+ *
+ * @author slawek
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prubujemy skonfigurować |GITHUB-a");
+    }
+}
