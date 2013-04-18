0f4f80b761d8cbc0093e78b0a4c70f6d6826b5bd
slawek <slawek@dom-11b457819a9>
2013-04-18 16:57:16 +0200

    Dodanie komentarza

--- Hello/src/hello/Hello.java
+++ Hello/src/hello/Hello.java
@@ -16,6 +16,7 @@ public class Hello {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //dodanie komentarza
         System.out.println("Prubujemy skonfigurować |GITHUB-a");
     }
 }
